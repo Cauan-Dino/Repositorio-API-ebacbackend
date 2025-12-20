8e439a4c8d4ddb8dc0f258ac53aab261f0413c41
Cauan Penha <cauan@gmail.com>
2025-12-20 15:04:19 -0300

    Adicionando autenticacao

--- projeto_API_Backend.py
+++ projeto_API_Backend.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,Depends
+from fastapi.security import HTTPBasicCredentials,HTTPBasic
 from pydantic import BaseModel
 from typing import Optional
+import secrets
 
 
 app = FastAPI(
@@ -20,31 +22,54 @@ class Livro(BaseModel):
     ano_lancamento: int 
     nome_autor: str
 
+# Senha e Usuario para fazer login
+MINHA_SENHA = 'admin'
+MEU_USUARIO = 'admin'
+
+# security eh um objeto da class HTTPBasic que permite com que o usuario consiga digitar sua senha e login no http basic
+security = HTTPBasic()
+
+# Funcao para permitir que o usuario consiga colocar a senha e o usuario
+def autenticar_meu_usuario(crenditials: HTTPBasicCredentials = Depends(security)):
+    is_username_correct = secrets.compare_digest(crenditials.username,MEU_USUARIO)
+    is_password_correct = secrets.compare_digest(crenditials.password,MINHA_SENHA)
+
+    if not (is_password_correct and is_username_correct):
+        raise HTTPException(
+            status_code=401,
+            detail='Usuario ou senha incorretos',
+            headers={'WWW-Authenticate':'Basic'}
+        )
+
+
 # 1. Criar um get para ver os livros que estao cadastrados
 @app.get('/livros')
-def get_meus_livros():
+def get_meus_livros(credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)):
     if not meus_livros:
         return {'message':'Ainda nao ha nada cadastrado'}
     else:
         return {'livros':meus_livros}
 
+
 # 2. Criar um post para adicionar um livro
 @app.post('/adicionar_livro')
-def post_meus_livros(id_livro:int,livro: Livro):
+def post_meus_livros(id_livro:int,livro: Livro,credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)):
     if id_livro in meus_livros:
         raise HTTPException(status_code=400,detail='Esse livro ja esta cadastrado')
     else:
         meus_livros[id_livro] = livro
         return {'message':f'Livro {livro.nome_livro} cadastrado com sucesso'}
 
+
 # 3. Criar um put para atualizar um livro ja existente
 @app.put('/atualizar_livro/{id_livro}')
-def put_meus_livros(id_livro:int,livro: Livro):
+def put_meus_livros(id_livro:int,livro: Livro,credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)):
     if id_livro not in meus_livros:
         raise HTTPException(status_code=404,detail='Esse livro nao existe')
     else:
         meus_livros[id_livro] = livro
         return {'message':f'Livro atualizado com sucesso'}
+
 
 # 4. Criar um delete
 @app.delete('/deletar_livro/{id_livro}')
